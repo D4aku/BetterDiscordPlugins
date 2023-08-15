2e8b825e00608854cf7b3a849d921b15af86b81b
ダーク。 <142275218+D4aku@users.noreply.github.com>
2023-08-15 03:45:57 +0200

    MorseConverter.plugin.js

--- Plugins/MorseConverter/MorseConverter.plugin.js
+++ Plugins/MorseConverter/MorseConverter.plugin.js
@@ -56,7 +56,14 @@ module.exports = (() => {
         const { LibraryUtils, ApplicationCommandAPI, BunnyLib } = ZLibrary.buildPlugin(config);
 
         const MorseCodeMap = {
-          // Morse code mapping here
+          A: ".-", B: "-...", C: "-.-.", D: "-..", E: ".",
+          F: "..-.", G: "--.", H: "....", I: "..", J: ".---",
+          K: "-.-", L: ".-..", M: "--", N: "-.", O: "---",
+          P: ".--.", Q: "--.-", R: ".-.", S: "...", T: "-",
+          U: "..-", V: "...-", W: ".--", X: "-..-", Y: "-.--",
+          Z: "--..", " ": "/", "1": ".----", "2": "..---",
+          "3": "...--", "4": "....-", "5": ".....", "6": "-....",
+          "7": "--...", "8": "---..", "9": "----.", "0": "-----"
         };
 
         return class Morse extends Plugin {
